085e1058aa9811ac0a4e3dde15e88339409503a8
Aryan Sethi <46134586+neohacker18@users.noreply.github.com>
2022-05-18 13:43:00 +0530

    Final touches

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -3,7 +3,7 @@ import React,{useState} from 'react'
 export default function TextForm(props) {
     const handleChangeClick=()=>{
       let newText=text.toUpperCase();
-      if(text==newText)
+      if(text===newText)
       {
         let ntext=text.toLowerCase();
         setText(ntext);
@@ -41,10 +41,10 @@ export default function TextForm(props) {
     const [text,setText]=useState('');
     return (
       <>
-      <div className="container" style={{color: props.mode=='dark'?'white':'black'}}>
+      <div className="container" style={{color: props.mode==='dark'?'white':'black'}}>
           <h1>{props.heading}</h1>
     <div className="mb-3">
-      <textarea className="form-control" value={text} style={{backgroundColor:props.mode=='light'?'white':'grey',color: props.mode=='dark'?'white':'black'}} onChange={handleOnChange} id="myBox" rows="8"></textarea>
+      <textarea className="form-control" value={text} style={{backgroundColor:props.mode==='light'?'white':'grey',color: props.mode==='dark'?'white':'black'}} onChange={handleOnChange} id="myBox" rows="8"></textarea>
     </div>
     <button className="btn btn-dark mx-2" onClick={handleChangeClick}>A/a</button>
     <button className="btn btn-dark mx-2" onClick={handleClearClick}>Clear</button>
@@ -52,10 +52,10 @@ export default function TextForm(props) {
     <button className="btn btn-dark mx-2" onClick={handleCopy}>Copy</button>
     <button className="btn btn-dark mx-2" onClick={removeSpace}>Remove Extra Spaces</button>
       </div>
-      <div className="container my-3" style={{color: props.mode=='dark'?'white':'black'}}>
+      <div className="container my-3" style={{color: props.mode==='dark'?'white':'black'}}>
         <h3>Your Text Summary</h3>
         <p>{text.length>0?text.trim().split(" ").length:0} words , {text.length} characters</p>
-        <p>{text==""?0:text.split(" ").length*0.08} Minutes to read</p>
+        <p>{text===""?0:text.split(" ").length*0.08} Minutes to read</p>
         <h3>Preview</h3>
         <p>{text.length>0?text:"Enter something in the textbox above to preview it here."}</p>
       </div>
